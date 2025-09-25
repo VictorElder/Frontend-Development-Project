93dda862fb583e89e7ac2c1705136016ac932872
Victor <eldervic001@gmail.com>
2025-09-25 14:32:10 +0100

    Finished UI

--- js/script.js
+++ js/script.js
@@ -1,6 +1,43 @@
-const slider = document.querySelector(".slider")
+let slider = document.querySelector(".slider")
 const nextBtn = document.querySelector(".slider_btn")
 const prevBtn = document.querySelector(".prev_btn")
+let cartEl = document.getElementById("cart-el")
+let sidebarEl = document.getElementById("sidebar-el")
+let sideContent = document.getElementById("side-content")
+const activePage = window.location.pathname
+
+
+function addToCart() {
+    alert("item has been added to cart")
+}
+
+
+
+cartEl.addEventListener('click', function(e){
+    e.preventDefault()
+    sidebarEl.style.display = 'flex';
+})
+
+window.addEventListener('wheel', function(e){
+    
+    eventHandler(e)
+})
+
+window.addEventListener('click', function(e){
+    
+    eventHandler(e)
+})
+
+function eventHandler(e) {
+    if(e.target === sidebarEl){
+        sidebarEl.style.display = 'none'
+    }
+}
+
+
+
+
+console.log(activePage)
 //let currentIndex = 0
 
 
@@ -17,14 +54,13 @@ slider.addEventListener("wheel", (e)=> {
     slider.scrollLeft += e.deltaY
 })
 
+
 nextBtn.addEventListener("click", ()=> {
     slider.style.scrollBehaviour = "smooth"
-    slider.scrollLeft += 300
+    slider.scrollLeft += 300;
 })
- prevBtn.addEventListener("click", ()=> {
-     slider.scrollLeft -= 250
- })
 
-function addToCart() {
-    alert("item has been added to cart")
-}
+
+ prevBtn.addEventListener("click", ()=> {
+     slider.scrollLeft -= 250;
+ })

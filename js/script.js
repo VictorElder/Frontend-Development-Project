46f1bd03c31c9e97a15a869902ed853e1fd00488
Victor <eldervic001@gmail.com>
2025-10-01 15:19:45 +0100

    begin interaction

--- js/script.js
+++ js/script.js
@@ -4,18 +4,43 @@ const prevBtn = document.querySelector(".prev_btn")
 let cartEl = document.getElementById("cart-el")
 let sidebarEl = document.getElementById("sidebar-el")
 let sideContent = document.getElementById("side-content")
+let addToCart = document.querySelectorAll(".add-to-cart")
+let notificationAlert = document.getElementById("notification")
+let notificationText = document.getElementById("notification-text")
 const activePage = window.location.pathname
 
+let count = 0
 
-function addToCart() {
-    alert("item has been added to cart")
+
+for(i = 0; i<addToCart.length; i++){
+    
+    addToCart[i].addEventListener('click', function(){
+        
+        notificationAlert.style.display = "block" ;k
+        notificationText.style.display = "block";
+        
+        setTimeout(function(){
+            notificationText.style.display = "none"
+        }, 1000);
+        
+        increment()
+        
+    })}
+    
+    function increment(){
+       count += 1
+        
+        notificationAlert.textContent = count
 }
-
 
 
 cartEl.addEventListener('click', function(e){
     e.preventDefault()
     sidebarEl.style.display = 'flex';
+    count = 0 ;
+    notificationAlert.textContent = count ;
+    
+    notificationAlert.style.display = ("none")
 })
 
 window.addEventListener('wheel', function(e){
